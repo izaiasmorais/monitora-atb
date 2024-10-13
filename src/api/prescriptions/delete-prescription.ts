9e3a1230e20cec733f4954f33eab644e37f1cf45
Izaias Lima <izaiaslima356@gmail.com>
2024-10-13 13:44:07 -0300

    fix: remove deprecated import

--- src/api/prescriptions/delete-prescription.ts
+++ src/api/prescriptions/delete-prescription.ts
@@ -1,5 +1,4 @@
 import { api } from "@/lib/axios";
-import type { DeletePrescriptionParams } from "@/models/prescription";
 
 export async function deletePrescription(id: string) {
 	await api.delete(`/prescriptions/${id}`);
